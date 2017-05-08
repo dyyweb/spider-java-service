774732aedaa92d0cf791f5a5691e8919d3b709c3
dyyweb <dyyweb@163.com>
2017-05-08 17:20:41 +0800

    dubbo java 包骨架

--- spider-settle-service/src/main/java/tf56/spider/settle/service/impl/SettleServiceImpl.java
+++ spider-settle-service/src/main/java/tf56/spider/settle/service/impl/SettleServiceImpl.java
@@ -3,6 +3,7 @@ package tf56.spider.settle.service.impl;
 import com.alibaba.dubbo.config.annotation.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.transaction.annotation.Transactional;
 import tf56.spider.settle.service.SettleService;
 
 import java.util.Date;
@@ -11,12 +12,15 @@ import java.util.Date;
 /**
  * Created by dy on 16-4-18.
  */
-@org.springframework.stereotype.Service
+//@org.springframework.stereotype.Service
 @Service
 public class SettleServiceImpl implements SettleService{
     private static final Logger logger = LoggerFactory.getLogger(SettleServiceImpl.class);
-    public String doSeckill(String name, Date time){
+    @Transactional(rollbackFor = Exception.class)
+    public String doSeckill(String name, Date time) throws Exception {
         logger.info("SettleService name："+name+", date："+ time);
+        int i = 1/0;
+        logger.info("================="+i);
         return "success by DY ,settleService 系统返回";
     }
 }
