4165c5f8fe124a052950c0075b91552e823fc87a
dyyweb <dyyweb@163.com>
2017-05-08 10:19:57 +0800

    dubbo java包骨架

--- spider-settle-service/src/main/java/tf56/spider/settle/SpiderSettleService.java
+++ spider-settle-service/src/main/java/tf56/spider/settle/SpiderSettleService.java
@@ -1,17 +1,19 @@
 package tf56.spider.settle;
 
+import ch.qos.logback.ext.spring.LogbackConfigurer;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class SpiderSettleService {
     public static void main(String[] args) {
         try {
+            LogbackConfigurer.initLogging("classpath:conf/logback.xml");
             new ClassPathXmlApplicationContext("classpath:conf/dubbo-settle-service.xml");
             System.out.println("***********服务启动成功***********");
             while (true) {
                 Thread.sleep(1000);
             }
-        } catch (InterruptedException e) {
+        } catch (Exception e) {
             System.out.println("xxxxxxxxxxxx服务启动失败xxxxxxxxxxxx");
             e.printStackTrace();
         }
